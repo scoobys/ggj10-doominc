437148c294907b20131a5b79c21ca6ba87ad1e67
Reimo Rebane <reimo.rebane@cyber.ee>
2018-01-27 20:01:16 +0200

    Added script for the sky prefab.

---
+++ Assets/Scripts/SkyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour {
+
+    Animator thisAnimator;
+
+    void Awake () {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    public void NextDay() {
+        thisAnimator.SetTrigger("NextDay");
+    }
+}
