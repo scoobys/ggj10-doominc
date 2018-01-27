fdab8326ea947a8aa769c2127b9ea08a48a48ff7
Reimo Rebane <reimo.rebane@cyber.ee>
2018-01-27 22:53:06 +0200

    Work-in-progress on newspaper.

---
+++ Assets/Scripts/NewspaperController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewspaperController : MonoBehaviour {
+
+    Animator thisAnimator;
+    TextMeshProUGUI nameText;
+    TextMeshProUGUI headlineText;
+
+    void Awake () {
+        thisAnimator = GetComponent<Animator>();
+        Component[] components = GetComponentsInChildren(typeof(TextMeshProUGUI));
+        foreach (Component c in components) {
+            string name = c.gameObject.name;
+            if (name == "Name") {
+                nameText = (TextMeshProUGUI) c;
+            } else if (name == "Header") {
+                headlineText = (TextMeshProUGUI) c;
+            }
+        }
+    }
+
+    public void SetName(string name) {
+        nameText.text = name;
+    }
+
+    public void Open(string headline) {
+        headlineText.text = headline;
+        thisAnimator.SetTrigger("Open");
+    }
+
+    public void Close() {
+        thisAnimator.SetTrigger("Close");
+    }
+}
