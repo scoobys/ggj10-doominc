10d817171ea947682a8f56213e92adc45e12809a
Frank Koppel <frank.koppel@itcollege.ee>
2018-01-28 12:03:44 +0200

    menu progress

---
+++ Assets/Scripts/Navigation/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Navigation
+{
+    public class MainMenu : MonoBehaviour
+    {
+        private MenuState _state;
+        private GameObject _main;
+        private GameObject _credits;
+        private GameObject _new;
+
+        public void Awake()
+        {
+            _main = GameObject.FindGameObjectWithTag("Main");
+            _credits = GameObject.FindGameObjectWithTag("Credits");
+            _new = GameObject.FindGameObjectWithTag("New");
+        }
+
+        public void Start()
+        {
+            _state = MenuState.Main;
+            Navigate(_state);
+        }
+
+        private void Navigate (MenuState newState)
+        {
+            _credits.SetActive(false);
+            _main.SetActive(false);
+            _new.SetActive(false);
+            switch (newState)
+            {
+                case MenuState.Main:
+                    _main.SetActive(true);
+                    break;
+                case MenuState.New:
+                    _new.SetActive(true);
+                    break;
+                case MenuState.Credits:
+                    _credits.SetActive(true);
+                    break;
+            }
+        }
+
+    }
+}
