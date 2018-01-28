76147108edbf8c39ce54f0aab0854595383d7105
Frank Koppel <frank.koppel@itcollege.ee>
2018-01-28 13:09:53 +0200

    progress

--- Assets/Scripts/Navigation/LoadSceneOnClick.cs
+++ Assets/Scripts/Navigation/LoadSceneOnClick.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
- public class LoadSceneOnClick : MonoBehaviour
+public class LoadSceneOnClick : MonoBehaviour
 {
-    public int TargetIndex;
+    public InputField _input;
+    public Game _global;
+
+
+    private void Awake()
+    {
+        _input = GameObject.FindGameObjectWithTag("Input").GetComponentInChildren<InputField>();
+        _global = GameObject.FindGameObjectWithTag("Global").GetComponentInChildren<Game>(); ;
+
+    }
 
     public void LoadByIndex(int sceneIndex)
     {
+        _global.villageName = _input.text;
+        Debug.Log(_global.villageName);
         SceneManager.LoadScene(sceneIndex);
     }
 }
