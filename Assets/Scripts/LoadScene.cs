f744ee6fdc60db8178a539817a8723d947502b74
Reimo Rebane <reimo.rebane@cyber.ee>
2018-01-28 16:06:33 +0200

    WIP

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public static void LoadByIndex(int sceneIndex)
+    {
+        Debug.Log("LoadScene " + sceneIndex);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
