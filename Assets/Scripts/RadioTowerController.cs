726e461748d0060c0d3c466382832f0859a0ad49
binoternary <binoternary@gmail.com>
2018-01-28 16:31:32 +0200

    Display transmissions (script)

--- Assets/Scripts/RadioTowerController.cs
+++ Assets/Scripts/RadioTowerController.cs
@@ -6,8 +6,9 @@ using TMPro;
 
 public class RadioTowerController : MonoBehaviour {
 
-    private Animator thisAnimator;
+    // private Animator thisAnimator;
     private TextMeshPro towerText;
+    private GameObject transmissionBG;
     private bool isTransmissionInProgress = false;
     private float nextTransmissionCloseTime = float.PositiveInfinity;
     private float lastTransmissionTime;
@@ -26,7 +27,7 @@ public class RadioTowerController : MonoBehaviour {
             }
         }
 
-        thisAnimator = GetComponent<Animator>();
+        // thisAnimator = GetComponent<Animator>();
 
         lastTransmissionTime = 0.0f;
         rnd = new System.Random();
@@ -34,21 +35,45 @@ public class RadioTowerController : MonoBehaviour {
             new[] { "\r\n", "\r", "\n" },
             StringSplitOptions.None
         );
+
+        // transmissionBG = GetComponentInChildren<SpriteRenderer>();
+        transmissionBG = GameObject.FindGameObjectWithTag("TransmissionBG");
+        towerText.gameObject.SetActive(false);
+        transmissionBG.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        isTransmissionInProgress = true;
+        TransmissionClose();
+        towerText.text = "";
+        nextTransmissionCloseTime = 0.0f;
     }
 
     void Update () {
         float t = Time.time;
+        
         if (nextTransmissionCloseTime < t) {
+            Debug.Log("update close");
             TransmissionClose();
             nextTransmissionCloseTime = float.PositiveInfinity;
         }
         // else if (t - lastTransmissionTime > rnd.Next(10, 30))
-        else if (t - lastTransmissionTime > 3)
+        else if (t - lastTransmissionTime > 10)
         {
+            Debug.Log("update open");
+            towerText.gameObject.SetActive(true);
             lastTransmissionTime = t;
             string transmission = transmissions[rnd.Next(0, transmissions.Length - 1)];
-            // TransmissionOpen(transmission);
+            TransmissionOpen(transmission);
             Debug.Log(transmission);
+            towerText.text = transmission;
+            towerText.gameObject.SetActive(true);
+            transmissionBG.gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("update none");
         }
     }
 
@@ -59,14 +84,14 @@ public class RadioTowerController : MonoBehaviour {
         isTransmissionInProgress = true;
         towerText.text = text;
         nextTransmissionCloseTime = Time.time + Game.instance.transmissionOpenDuration;
-        thisAnimator.SetTrigger("Open");
+        // thisAnimator.SetTrigger("Open");
     }
 
     public void TransmissionClose() {
         if (!isTransmissionInProgress) {
             return;
         }
-        thisAnimator.SetTrigger("Close");
+        // thisAnimator.SetTrigger("Close");
         isTransmissionInProgress = false;
     }
 }
