bbbdb0686b444f6f1698b7645d1980e4c07c1706
Frank Koppel <frank.koppel@itcollege.ee>
2018-01-28 14:21:15 +0200

    commit

---
+++ Assets/Scripts/CurrentHouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public enum CurrentHouse
+    {
+        None,
+        Church,
+        Tavern,
+        MayorEstate,
+        Brothel,
+        Alchemist,
+        Market
+    }
+}
