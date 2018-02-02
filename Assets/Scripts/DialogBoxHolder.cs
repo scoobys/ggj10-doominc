d11496c0f552839f377e99623637f84bbfcc5b80
Frank <Frank.koppel@itcollege.ee>
2018-02-02 07:38:01 +0200

    try1

--- Assets/Scripts/DialogBoxHolder.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using UnityEngine;
-using TMPro;
-
-public class DialogBoxHolder : MonoBehaviour {
-
-	public GameObject dialogBox;
-	public TextMeshProUGUI questText;
-}
---
+++ Assets/Scripts/Menu/ButtonBase.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Menu
+{
+    public class ButtonBase : MonoBehaviour
+    {
+        public AudioSource ClickSound;
+        public Scenes TargetScene = Scenes.MainMenu;
+
+        public void LoadScene()
+        {
+            if (ClickSound != null)
+            {
+                ClickSound.Play();
+            }
+            SceneManager.LoadScene((int)TargetScene);
+        }
+
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+        }
+    }
+}
